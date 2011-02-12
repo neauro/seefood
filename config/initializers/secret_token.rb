2229e9843c375ed711a925b2bb7b3de24cc4cf28
Joshua Hou <joshuahou@gmail.com>
2011-02-11 18:41:42 -0800

    initial rails app commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Seefood::Application.config.secret_token = 'ba640aee6c0cdc77ec5c4f471ba0ecf53a4a186a238d61069c2caa9b6c766e67efe2213723dbd851fb532801a08797707da6a1d82115f51b6bf77f0aba9cc4b4'
